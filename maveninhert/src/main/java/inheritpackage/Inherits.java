40c5e97f95cb435614d231986e9e326559b6149e
MIDHUN M <MIDHUN M@LAPTOP-BGGC3FN3>
2023-07-25 12:18:31 +0530

    mickey

---
+++ maveninhert/src/main/java/inheritpackage/Inherits.java
@@ -0,0 +1,11 @@
+package inheritpackage;
+
+public class Inherits {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Mickey");
+		// TODO Auto-generated method stub
+
+	}
+
+}
